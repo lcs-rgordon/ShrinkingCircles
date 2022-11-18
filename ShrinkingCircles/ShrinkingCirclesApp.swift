f8976efe3d537d3fa1d8cc0766a79d9dfdb0147a
Russell Gordon <rgordon@lcs.on.ca>
2022-11-18 14:18:24 -0500

    Initial Commit

---
+++ ShrinkingCircles/ShrinkingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrinkingCirclesApp.swift
+//  ShrinkingCircles
+//
+//  Created by Russell Gordon on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ShrinkingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
